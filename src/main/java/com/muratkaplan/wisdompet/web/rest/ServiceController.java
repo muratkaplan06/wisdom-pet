23adcb19e10484b4de0af4ff2f163c09ea4a4537
Murat Kaplan <muratkaplan061989@gmail.com>
2023-10-16 00:19:47 +0300

    controller

--- src/main/java/com/muratkaplan/wisdompet/web/rest/ServiceController.java
+++ src/main/java/com/muratkaplan/wisdompet/web/rest/ServiceController.java
@@ -1,7 +1,45 @@
 package com.muratkaplan.wisdompet.web.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.muratkaplan.wisdompet.services.ServiceService;
+import com.muratkaplan.wisdompet.web.errors.BadRequestException;
+import com.muratkaplan.wisdompet.web.models.Service;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/services")
+@RequiredArgsConstructor
 public class ServiceController {
+    private final ServiceService serviceService;
+
+    @GetMapping
+    public List<Service> getAll(){
+        return serviceService.getAllServices();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Service create(@RequestBody Service service){
+        return serviceService.createOrUpdateService(service);
+    }
+    @GetMapping("/{id}")
+    public Service getService(@PathVariable("id") Long id){
+        return serviceService.getServiceById(id);
+    }
+    @PutMapping("/{id}")
+    public Service updateService(@PathVariable("id") Long id,@RequestBody Service service){
+        if(!id.equals(service.getServiceId())){
+            throw new BadRequestException("Service id does not match");
+        }
+        service.setServiceId(id);
+        return serviceService.createOrUpdateService(service);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteService(@PathVariable("id") Long id){
+        serviceService.deleteServiceById(id);
+    }
 }
---
+++ src/main/java/com/muratkaplan/wisdompet/web/rest/VendorController.java
@@ -0,0 +1,44 @@
+package com.muratkaplan.wisdompet.web.rest;
+
+import com.muratkaplan.wisdompet.services.VendorService;
+import com.muratkaplan.wisdompet.web.errors.BadRequestException;
+import com.muratkaplan.wisdompet.web.models.Vendor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/vendors")
+public class VendorController {
+    private final VendorService vendorService;
+
+    @GetMapping
+    List<Vendor> getAll(){
+        return vendorService.getAllVendors();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Vendor create(@RequestBody Vendor vendor){
+        return vendorService.createOrUpdateVendor(vendor);
+    }
+    @GetMapping("/{id}")
+    public Vendor getVendor(@PathVariable("id") Long id){
+        return vendorService.getVendorById(id);
+    }
+    @PutMapping("/{id}")
+    public Vendor updateVendor(@PathVariable("id") Long id,@RequestBody Vendor vendor){
+        if(!id.equals(vendor.getVendorId())){
+            throw new BadRequestException("Vendor id does not match");
+        }
+        vendor.setVendorId(id);
+        return vendorService.createOrUpdateVendor(vendor);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteVendor(@PathVariable("id") Long id){
+        vendorService.deleteVendorById(id);
+    }
+}
